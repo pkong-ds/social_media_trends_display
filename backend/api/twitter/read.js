16edd7b25e8eee791be4816b01768e5e7c2a4fb4
pkong-ds <pkong.work@gmail.com>
2022-02-22 00:27:59 +0100

    refactor(api): remove outdated comments

--- backend/api/twitter/read.js
+++ backend/api/twitter/read.js
@@ -15,30 +15,4 @@ export const readTwitterDataFromDb = async () => {
     console.error(err);
     return [];
   }
-
-  // .toArray((err, res) => {
-  //   if (err) throw err;
-  //   console.log({ res });
-  //   console.log("Successfully retrived twitter trends from database.");
-  //   return res;
-  // });
 };
-// export const writeTwitterDataToDb = async () => {
-//   const data = await fetchTwitterApi();
-//   const { query, trends } = parseTwitterData(data);
-//   clearTwitterCollections();
-//   let dbConnect = getDb();
-//   dbConnect.collection("TwitterTrends").insertMany(trends, (err, res) => {
-//     if (err) throw err;
-//     console.log(`Inserted *${res.insertedCount}* documents`);
-//   });
-//   return trends;
-// };
-
-// const clearTwitterCollections = () => {
-//   let dbConnect = getDb();
-//   dbConnect.collection("TwitterTrends").drop((err, delOK) => {
-//     if (err) throw err;
-//     if (delOK) console.log("Collection 'trends' deleted");
-//   });
-// };
